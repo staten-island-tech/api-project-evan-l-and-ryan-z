4aeb1099656faeff9afe3ae7d74ca8cc9591dceb
evanleung23 <71602688+evanleung23@users.noreply.github.com>
2021-05-11 21:40:46 -0400

    basic api key retrieval

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,22 @@
-import { DOMSelectors } from "./DOM";
-import { genres } from "./genre";
+const key =
+  "https://api.worldofwarships.com/wows/account/list/?application_id=fde297950d8345d873843e777c3650f4&search=stabbyshpee";
+const playerName = document.getElementById("playerName");
+const playerID = document.getElementById("playerID");
 
-const key = "YOURKEYHERE";
+console.log(key);
+const apiInformation = async function () {
+  try {
+    const retrieve = await fetch(key);
+    const data = await retrieve.json();
+    console.log(data.data[0]);
+    function displayInfo() {
+      playerName.innerText = data.data[0].nickname;
+      playerID.innerText = data.data[0].account_id;
+    }
+    displayInfo();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+apiInformation();
