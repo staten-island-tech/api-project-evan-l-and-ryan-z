0e1de7ca374d20d045d23047e438f0c64f4675cb
evanleung23 <71602688+evanleung23@users.noreply.github.com>
2021-05-25 15:00:35 -0400

    displayed player information

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,42 @@
 const key =
   "https://api.worldofwarships.com/wows/account/list/?application_id=fde297950d8345d873843e777c3650f4&search=StabbyShpee";
+const key2 =
+  "https://api.worldofwarships.com/wows/account/info/?application_id=fde297950d8345d873843e777c3650f4&account_id=1033734436";
 const playerName = document.getElementById("playerName");
 const playerID = document.getElementById("playerID");
 const userInputBox = document.getElementById("userInputBox");
 const submitBtn = document.getElementById("submitBtn");
+const playerBattles = document.getElementById("playerBattles");
+const playerKills = document.getElementById("playerKills");
+const damageDealt = document.getElementById("damageDealt");
+const wins = document.getElementById("wins");
+const losses = document.getElementById("losses");
+const draws = document.getElementById("draws");
 
 console.log(key);
-
-submitBtn.addEventListener("click", configureInput);
-
-function configureInput(e) {
-  e.preventDefault();
-  console.log(userInputBox.value);
-  let userInput = userInputBox.value;
-}
-
-console.log(userInputBox.type);
+console.log(key2);
 
 const apiInformation = async function () {
   try {
-    const retrieve = await fetch(key);
-    const data = await retrieve.json();
+    const response = await fetch(key);
+    const data = await response.json();
     console.log(data.data[0]);
+    const specifiedID = data.data[0]["account_id"];
+    console.log(specifiedID);
+    const response2 = await fetch(key2);
+    const data2 = await response2.json();
+    console.log(specifiedID);
+    const stats = data2["data"][specifiedID]["statistics"]["pvp"];
+    console.log(stats);
     function displayInfo() {
-      playerName.innerText = data.data[0].nickname;
-      playerID.innerText = data.data[0].account_id;
+      playerName.innerText = data.data[0]["nickname"];
+      playerID.innerText = specifiedID;
+      playerBattles.innerText = stats.battles;
+      playerKills.innerText = stats.frags;
+      damageDealt.innerText = stats.damage_dealt;
+      wins.innerText = stats.wins;
+      losses.innerText = stats.losses;
+      draws.innerText = stats.draws;
     }
     displayInfo();
   } catch (error) {
