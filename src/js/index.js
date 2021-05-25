81a33c02e2d6fe3e81c1c017956380059e686768
evanleung23 <71602688+evanleung23@users.noreply.github.com>
2021-05-25 13:20:06 -0400

    progress 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,22 @@
 const key =
-  "https://api.worldofwarships.com/wows/account/list/?application_id=fde297950d8345d873843e777c3650f4&search=stabbyshpee";
+  "https://api.worldofwarships.com/wows/account/list/?application_id=fde297950d8345d873843e777c3650f4&search=StabbyShpee";
 const playerName = document.getElementById("playerName");
 const playerID = document.getElementById("playerID");
+const userInputBox = document.getElementById("userInputBox");
+const submitBtn = document.getElementById("submitBtn");
 
 console.log(key);
+
+submitBtn.addEventListener("click", configureInput);
+
+function configureInput(e) {
+  e.preventDefault();
+  console.log(userInputBox.value);
+  let userInput = userInputBox.value;
+}
+
+console.log(userInputBox.type);
+
 const apiInformation = async function () {
   try {
     const retrieve = await fetch(key);
